bab53dcdb94dc941b907c6166d280e13ed988768
Javi Romero <javirg1@gmail.com>
2020-10-13 12:45:09 +0200

    Se muestra la foto por defecto en detalle_anuncio.php

--- 26_bbdd_anuncios - javi/publica/detalle_anuncio.php
+++ 26_bbdd_anuncios - javi/publica/detalle_anuncio.php
@@ -28,6 +28,7 @@ Formulario para VER el detalle de cada anuncio de la zona pública
         <h1>WEB DE ANUNCIOS - COMPRA y VENTA</h1>
         <p>Si quieres poner tus anuncios, <a href="login.php">identifícate</a></p>
         <hr>
+        <img src="../fotos/default.png" width="80" />
         <h3><span style=color:blue>Artículo: </span><?php echo $_SESSION["titulo"] ?></h3>
         <h3><span style=color:blue>Descripción: </span><?php echo $_SESSION["descripcion"] ?></h3>
         <h3><span style=color:blue>Precio: </span><?php echo $_SESSION["precio"] ?></h3>
