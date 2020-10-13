1da71e4c9c8335fd69852275f65ebb2a01da8a7b
Javi Romero <javirg1@gmail.com>
2020-10-13 10:49:02 +0200

    Se añade funcionalidad para ver el detalle da cada anuncio desde el index de la zona pública

--- 26_bbdd_anuncios - javi/publica/detalle_anuncio.php
+++ 26_bbdd_anuncios - javi/publica/detalle_anuncio.php
@@ -5,22 +5,13 @@
 // ************************************************************************************
 
 session_start();
-//Vamos a comprobar si existe una variable de sesión que contenga el id de usuario:
-if (isset($_SESSION["id_usuario"])) {
-    //El usuario está identificado. Podría hacer una consulta y obtener todos sus datos
-    $id_usuario = $_SESSION["id_usuario"];
-    $nombre_usuario = $_SESSION["nombre_usuario"];
-} else {
-    //El usuario se está intentando colar, no se ha identificado:
-    header("Location:../publica/login.php?error=Es necesario identificarse");
-    exit;
-}
+
 ?>
 
 <!--*************************************************************************************
 VISTA
 Zona Privada
-Formulario para EDITAR / ACTUALIZAR anuncios
+Formulario para VER el detalle de cada anuncio de la zona pública
 *****************************************************************************************-->
 
 <!DOCTYPE html>
@@ -29,18 +20,21 @@ Formulario para EDITAR / ACTUALIZAR anuncios
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=b, initial-scale=1.0">
-    <title>Anuncios - Editar</title>
+    <title>Anuncios - Publicados</title>
 </head>
 
 <body>
     <center>
         <h1>WEB DE ANUNCIOS - COMPRA y VENTA</h1>
-        <h2>Zona privada de <span style=color:forestgreen><?php echo $nombre_usuario ?></span></h2>
-        <!-- <p><?php echo $_SESSION["id_anuncio"] ?></p> -->
-        <p><?php echo "Artículo: ".$_SESSION["titulo"] ?></p>
-        <p><?php echo "Descripción: ".$_SESSION["descripcion"] ?></p>
-        <p><?php echo "Precio: ".$_SESSION["precio"] ?></p>
-        <p><?php echo "Fecha de publicación: ".$_SESSION["fecha"] ?></p>
+        <p>Si quieres poner tus anuncios, <a href="login.php">identifícate</a></p>
+        <hr>
+        <h3><span style=color:blue>Artículo: </span><?php echo $_SESSION["titulo"] ?></h3>
+        <h3><span style=color:blue>Descripción: </span><?php echo $_SESSION["descripcion"] ?></h3>
+        <h3><span style=color:blue>Precio: </span><?php echo $_SESSION["precio"] ?></h3>
+        <h3><span style=color:blue>Fecha de publicación: </span><?php echo $_SESSION["fecha"] ?></h3>
+        <hr>
+        <p>Para volver al listado de anuncios, haz click <a href="index.php">aquí</a></p>
+        
     </center>
 </body>
 
